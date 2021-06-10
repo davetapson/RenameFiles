73c5e7d664d2d79d5d764aa78f311a1e09af7b1b
Dave Tapson <dave.tapson@gmail.com>
2021-06-10 21:16:08 +1200

    Add project files.

---
+++ RenameFiles/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RenameFiles
+{
+    public partial class frmMain : Form
+    {
+        List<string> fileNames = new List<string>();
+
+        public frmMain()
+        {
+            InitializeComponent();
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            txtFileName.Text = DateTime.Now.ToString("yyyy-MM-dd") + " ";
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            
+            List<int> ints = new List<int>();
+            openFileDialog.Title = "Select image files";
+            openFileDialog.Filter = "jpg files (*.jpg)|*.jpg";
+            openFileDialog.CheckFileExists = true;
+            openFileDialog.CheckPathExists = true;
+            openFileDialog.Multiselect = true;
+            openFileDialog.DefaultExt = "jpg";
+            openFileDialog.FileName = txtFileName.Text;
+            if (openFileDialog.ShowDialog() == DialogResult.OK) 
+            {
+                fileNames = openFileDialog.FileNames.ToList();
+                txtPath.Text = Path.GetDirectoryName(fileNames[0]);
+            };
+
+        }
+
+        private void btnRename_Click(object sender, EventArgs e)
+        {
+            if (fileNames == null || fileNames.Count == 0)
+            {
+                MessageBox.Show("No files to rename");
+            }
+            else
+            {
+                int max = GetMax();
+
+                string folder = Path.GetDirectoryName(fileNames[0]);
+
+
+                foreach( string file in fileNames)
+                {
+                    File.Move(file, folder + @"\" + txtFileName.Text + max.ToString().PadLeft(4, '0') + ".jpg");
+                    max++;
+                }
+
+                MessageBox.Show(fileNames.Count + " files renamed.", "Result", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                fileNames = new List<string>();
+            }
+
+        }
+
+        private int GetMax()
+        {
+            List<int> ints = new List<int>();
+
+            if (fileNames.Count > 0)
+            {
+                string folder = Path.GetDirectoryName(fileNames[0]);
+                string[] files = Directory.GetFiles(folder);
+               
+
+                if(files.Count() > 0)
+                {
+                    foreach(string file in files)
+                    {
+                        if (file.Contains(txtFileName.Text))
+                        {
+                            string intBit = Path.GetFileNameWithoutExtension(file).Replace(txtFileName.Text, "");
+                            
+                            int number;
+                            if (Int32.TryParse(intBit, out number))
+                            {
+                                ints.Add(number);
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (ints.Count > 0)
+            {
+                return ints.Max() + 1;
+            }
+            else return 0;
+        }
+    }
+}
---
+++ RenameFiles/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Rename Files")]
+[assembly: AssemblyDescription("Rename files in a folder")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Kowhai Software Solutions LTD")]
+[assembly: AssemblyProduct("Rename Files")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("656a6b41-f1fe-40fb-b60e-d477d38b3048")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.3")]
+[assembly: AssemblyFileVersion("1.0.0.3")]
